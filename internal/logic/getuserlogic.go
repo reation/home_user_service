125d0964964db83883b757e4a96c8bae9e74d807
reation_11 <reation_11@163.com>
2022-10-01 16:55:34 +0800

    tijiao

---
+++ internal/logic/getuserlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/reation/home_user_service/internal/svc"
+	"github.com/reation/home_user_service/proto/types/proto"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetUserLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserLogic {
+	return &GetUserLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *GetUserLogic) GetUser(in *proto.IdRequest) (*proto.UserInfoResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &proto.UserInfoResponse{}, nil
+}
